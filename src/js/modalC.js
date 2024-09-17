15281da45a6bf093bc88836ccb0d108819078352
CatalinaCap <c.cata1706@gmail.com>
2024-09-17 15:07:44 +0100

    one-part-modal

---
+++ src/js/modalC.js
@@ -0,0 +1,19 @@
+(function () {
+  const modal = document.getElementById('movieModal');
+  const openModalBtn = document.getElementById('openModalBtn');
+  const closeModalBtn = document.getElementsByClassName('close')[0];
+
+  openModalBtn.onclick = function () {
+    modal.style.display = 'block';
+  };
+
+  closeModalBtn.onclick = function () {
+    modal.style.display = 'none';
+  };
+
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  };
+})();
