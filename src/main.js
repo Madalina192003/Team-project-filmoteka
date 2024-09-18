7c7243e8d7ce41b0787e2e662cc6c527aabfd060
Madalina192003 <158836491+Madalina192003@users.noreply.github.com>
2024-09-18 14:53:23 +0300

    update main js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-
-"use strict";
+'use strict';
 
 import { getMovies } from './js/get-movies.js';
-
-import { testFetchMovieTrailer, testFetchMovies } from './js/testing-get-movies.js';
-
-import { nightMode } from "./js/nightMode";
-
-import {getLocalStorage} from "./js/getLocalStorage.js";
+import {
+  testFetchMovieTrailer,
+  testFetchMovies,
+} from './js/testing-get-movies.js';
+import { nightMode } from './js/nightMode.js';
+import {
+  addToStorage,
+  getFromStorage,
+  removeFromStorage,
+} from './js/getLocalStorage.js';
