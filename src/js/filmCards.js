9cd33f392980160720ef6e5e1b45f4c4b30178b4
Madalina192003 <158836491+Madalina192003@users.noreply.github.com>
2024-09-20 11:42:37 +0300

    update film card

--- src/js/filmCards.js
+++ src/js/filmCards.js
@@ -1,54 +1,61 @@
-import {fetchGenres} from './fetchGenres.js';
+import { fetchGenres } from './fetchGenres.js';
 import { getMovies } from './getMovies.js';
-
+import { openFilmModal } from './openFilmModal.js';
 
 function getGenreNames(genreIds) {
-    return genreIds
-      .map(id => genres.find(genre => genre.id === id)?.name || 'Unknown')
-      .filter(name => name !== 'Unknown')
-      .join(', ');
+  return genreIds
+    .map(id => genres.find(genre => genre.id === id)?.name || 'Unknown')
+    .filter(name => name !== 'Unknown')
+    .join(', ');
+}
+
+function getReleaseYear(response) {
+  if (response.media_type === 'movie') {
+    return response.release_date
+      ? response.release_date.split('-')[0]
+      : 'Unknown Year';
   }
-  
-  function getReleaseYear(response) {
-    if (response.media_type === 'movie') {
-      return response.release_date ? response.release_date.split('-')[0] : 'Unknown Year';
-    }
-    if (response.media_type === 'tv') {
-      return response.first_air_date ? response.first_air_date.split('-')[0] : 'Unknown Year';
-    }
-    return 'Unknown Year';
+  if (response.media_type === 'tv') {
+    return response.first_air_date
+      ? response.first_air_date.split('-')[0]
+      : 'Unknown Year';
   }
-  
-  function getTitle(response) {
-    if (response.media_type === 'tv') {
-      return response.name || response.original_name || 'Unknown Title';
-    }
-    return response.title || response.original_title || 'Unknown Title';
+  return 'Unknown Year';
+}
+
+function getTitle(response) {
+  if (response.media_type === 'tv') {
+    return response.name || response.original_name || 'Unknown Title';
   }
-  
-  export async function createFilmCard(dataPromise) {
-    const movieCard = document.querySelector('.movie-wrapper');
-    const data = await dataPromise;
-  
-    movieCard.textContent = ''; 
-  
-    if (!data?.results) return; 
-  
-    data.results.forEach(response => {
-      const movieElement = document.createElement('div');
-      movieElement.classList.add('movie-wrapper__card');
-  
-      const title = getTitle(response);
-      const genreNames = getGenreNames(response.genre_ids);
-      const releaseYear = getReleaseYear(response);
-      const rating = typeof response.vote_average === 'number' ? response.vote_average.toFixed(2) : 'N/A';
-      const posterPath = response.poster_path
-        ? `https://image.tmdb.org/t/p/w500${response.poster_path}`
-        : 'path/to/default/image.jpg';
-  
-      movieElement.setAttribute('data-filmId', response.id.toString());
-  
-      movieElement.innerHTML = `
+  return response.title || response.original_title || 'Unknown Title';
+}
+
+export async function createFilmCard(dataPromise) {
+  const movieCard = document.querySelector('.movie-wrapper');
+  const data = await dataPromise;
+
+  movieCard.textContent = '';
+
+  if (!data?.results) return;
+
+  data.results.forEach(response => {
+    const movieElement = document.createElement('div');
+    movieElement.classList.add('movie-wrapper__card');
+
+    const title = getTitle(response);
+    const genreNames = getGenreNames(response.genre_ids);
+    const releaseYear = getReleaseYear(response);
+    const rating =
+      typeof response.vote_average === 'number'
+        ? response.vote_average.toFixed(2)
+        : 'N/A';
+    const posterPath = response.poster_path
+      ? `https://image.tmdb.org/t/p/w500${response.poster_path}`
+      : 'path/to/default/image.jpg';
+
+    movieElement.setAttribute('data-filmId', response.id.toString());
+
+    movieElement.innerHTML = `
         <div class="movie-wrapper__card-img">
           <img src="${posterPath}" alt="${title}">
           <span class="movie-wrapper__info-rating">${rating}</span>
@@ -60,13 +67,12 @@ function getGenreNames(genreIds) {
             <span class="movie-wrapper__info-year"> | ${releaseYear}</span>
           </div>
         </div>`;
-  
-      movieCard.append(movieElement);
-  
-      movieElement.addEventListener('click', () => {
-        document.querySelector('.film-modal')?.remove();
-        openFilmModal(response, movieElement.outerHTML);
-      });
+
+    movieCard.append(movieElement);
+
+    movieElement.addEventListener('click', () => {
+      document.querySelector('.film-modal')?.remove();
+      openFilmModal(response, movieElement.outerHTML);
     });
-  }
-  
+  });
+}
