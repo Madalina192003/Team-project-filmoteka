12d2ee6ed5818cc7f2c7ebc5e694384d7c422eaa
Madalina192003 <158836491+Madalina192003@users.noreply.github.com>
2024-09-23 10:33:43 +0300

    update openfilmmodal

--- src/js/openFilmModal.js
+++ src/js/openFilmModal.js
@@ -3,7 +3,7 @@ import {
   addToStorage,
   getFromStorage,
   removeFromStorage,
-} from './setGetLocalStorage';
+} from './getLocalStorage';
 import {
   addToQueue,
   addToWatched,
@@ -14,24 +14,28 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 export const openFilmModal = (filmData, cardHtml) => {
   if (!filmData || typeof filmData !== 'object' || !filmData.genre_ids) {
-    console.error('Datele filmului nu sunt complete sau sunt într-un format incorect.');
+    console.error(
+      'Datele filmului nu sunt complete sau sunt într-un format incorect.'
+    );
     return;
   }
 
   // nume genurilor
-  const genreNames = (Array.isArray(filmData.genre_ids) && filmData.genre_ids.length > 0) 
-    ? filmData.genre_ids
-        .map(id => genres.find(genre => genre.id === id)?.name || 'Unknown')
-        .filter(name => name !== 'Unknown')
-        .join(', ')
-    : 'Unknown';
+  const genreNames =
+    Array.isArray(filmData.genre_ids) && filmData.genre_ids.length > 0
+      ? filmData.genre_ids
+          .map(id => genres.find(genre => genre.id === id)?.name || 'Unknown')
+          .filter(name => name !== 'Unknown')
+          .join(', ')
+      : 'Unknown';
 
   let originalTitle = filmData.title || 'Unknown Title';
 
   if (filmData.media_type) {
-    originalTitle = filmData.media_type === 'movie'
-      ? filmData.original_title || filmData.title
-      : filmData.original_name || filmData.name;
+    originalTitle =
+      filmData.media_type === 'movie'
+        ? filmData.original_title || filmData.title
+        : filmData.original_name || filmData.name;
   }
 
   //  titlul film și ID-ul
@@ -47,22 +51,29 @@ export const openFilmModal = (filmData, cardHtml) => {
         <div class="film-modal-content">
          <span class="close-modal">&times;</span>
          <h2 class="film-modal-title">${filmTitle}</h2>
-        ${filmData.trailerUrl
-          ? `<div class="film-modal-trailer">
+        ${
+          filmData.trailerUrl
+            ? `<div class="film-modal-trailer">
                <iframe src="${filmData.trailerUrl}" frameborder="0" allowfullscreen></iframe>
              </div>`
-          : `<div class="film-modal-poster">
+            : `<div class="film-modal-poster">
                <img src="https://image.tmdb.org/t/p/w500${filmData.poster_path}" alt="${filmTitle}">
              </div>`
         }
         <h3>Original title: ${originalTitle}</h3>
-        <p class="film-modal-score"><span>Score: ${typeof filmData.vote_average === 'number' ? filmData.vote_average.toFixed(2) : 'N/A'}</span></p>
+        <p class="film-modal-score"><span>Score: ${
+          typeof filmData.vote_average === 'number'
+            ? filmData.vote_average.toFixed(2)
+            : 'N/A'
+        }</span></p>
         <p class="modal-genre-paragraph"><b>Genre:</b> ${genreNames}</p>
         <h4 class="film-modal-about">ABOUT</h4>
         <p>${filmData.overview}</p>
         <div class="film-modal-actions">
           <button id="addToWatchedBtn">ADD TO WATCHED</button>
-          <button id="addToQueueBtn">${isFilmInQueue ? 'REMOVE FROM QUEUE' : 'ADD TO QUEUE'}</button>
+          <button id="addToQueueBtn">${
+            isFilmInQueue ? 'REMOVE FROM QUEUE' : 'ADD TO QUEUE'
+          }</button>
         </div>
       </div>
     </div>`;
@@ -77,8 +88,12 @@ export const openFilmModal = (filmData, cardHtml) => {
   const setButtonStates = () => {
     const filmObject = getFromStorage(filmData.id);
     if (filmObject) {
-      addToQueueBtn.textContent = filmObject.isInQueue ? 'REMOVE FROM QUEUE' : 'ADD TO QUEUE';
-      addToWatchedBtn.textContent = filmObject.isWatched ? 'REMOVE FROM WATCHED' : 'ADD TO WATCHED';
+      addToQueueBtn.textContent = filmObject.isInQueue
+        ? 'REMOVE FROM QUEUE'
+        : 'ADD TO QUEUE';
+      addToWatchedBtn.textContent = filmObject.isWatched
+        ? 'REMOVE FROM WATCHED'
+        : 'ADD TO WATCHED';
     }
   };
   setButtonStates();
@@ -86,7 +101,11 @@ export const openFilmModal = (filmData, cardHtml) => {
   addToQueueBtn.addEventListener('click', () => {
     const filmObject = getFromStorage(filmData.id);
     if (filmObject) {
-      updateFilmInStorage(filmData.id, !filmObject.isInQueue, filmObject.isWatched);
+      updateFilmInStorage(
+        filmData.id,
+        !filmObject.isInQueue,
+        filmObject.isWatched
+      );
       if (filmObject.isInQueue) {
         removeFilmCardFromDOM(filmData.id);
       }
@@ -99,7 +118,11 @@ export const openFilmModal = (filmData, cardHtml) => {
   addToWatchedBtn.addEventListener('click', () => {
     const filmObject = getFromStorage(filmData.id);
     if (filmObject) {
-      updateFilmInStorage(filmData.id, filmObject.isInQueue, !filmObject.isWatched);
+      updateFilmInStorage(
+        filmData.id,
+        filmObject.isInQueue,
+        !filmObject.isWatched
+      );
       if (filmObject.isWatched) {
         removeFilmCardFromDOM(filmData.id);
       }
@@ -116,7 +139,10 @@ export const openFilmModal = (filmData, cardHtml) => {
   });
 
   function handleModalClick(event) {
-    if (event.target === modal || event.target.classList.contains('close-modal')) {
+    if (
+      event.target === modal ||
+      event.target.classList.contains('close-modal')
+    ) {
       modal.remove();
       document.removeEventListener('click', handleModalClick);
       document.removeEventListener('keydown', handleEscapeKey);
@@ -137,7 +163,9 @@ export const openFilmModal = (filmData, cardHtml) => {
 
 function removeFilmCardFromDOM(filmId) {
   console.log(`Încercăm să eliminăm cardul cu ID-ul: ${filmId}`);
-  const filmCard = document.querySelector(`.movie-wrapper__card[data-filmid="${filmId}"]`);
+  const filmCard = document.querySelector(
+    `.movie-wrapper__card[data-filmid="${filmId}"]`
+  );
   if (filmCard) {
     filmCard.remove();
     console.log(`Cardul cu ID-ul: ${filmId} a fost eliminat.`);
